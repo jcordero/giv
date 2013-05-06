c0a1cee6a695bc3dba1514d3a82e2a9c60939400
Gaston Zanitti <gzanitti@gmail.com>
2013-05-06 13:19:58 -0300

    Monitoreo

--- www/lmodules/monitoreo/monitoreo.sql
+++ www/lmodules/monitoreo/monitoreo.sql
@@ -30,3 +30,30 @@ CREATE TABLE motivos_de_cierre_forzado (
 		id_motivo_de_cierre_forzado
 	)
 )
+
+CREATE TABLE monitoreos (
+  id_monitoreo int(10) unsigned NOT NULL AUTO_INCREMENT,
+  id_supervisor int(10) unsigned NOT NULL,
+  id_operador int(10) unsigned NOT NULL,
+  id_circuito int(10) unsigned NOT NULL,
+  codigo_de_referencia varchar(50) DEFAULT NULL,
+  fecha_de_monitoreo datetime NOT NULL,
+  resultado tinyint(10) unsigned NOT NULL,
+  comentarios text,
+  fecha_de_creacion datetime NOT NULL,
+  constraint pk_monitoreos PRIMARY KEY clustered 
+	(
+		id_monitoreo
+	)
+)
+
+CREATE TABLE items_de_monitoreo (
+  id_item_de_monitoreo int(10) unsigned NOT NULL AUTO_INCREMENT,
+  id_monitoreo int(10) unsigned NOT NULL DEFAULT '0',
+  id_item_a_monitorear int(10) unsigned NOT NULL DEFAULT '0',
+  resultado tinyint(3) unsigned NOT NULL DEFAULT '0',
+  constraint pk_items_de_monitoreo PRIMARY KEY clustered 
+	(
+		id_item_de_monitoreo
+	)
+)
